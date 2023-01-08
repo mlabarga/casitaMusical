31259c4b2cfc591765a3f927d144b240360f821c
mayte <mayte_labarga@hotmail.com>
2023-01-08 13:19:45 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+let kontroller = 0
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P12, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
 basic.forever(function () {
-	
+    kontroller = pins.analogReadPin(AnalogPin.P0)
+    if (kontroller < 200) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else if (kontroller >= 220 && kontroller <= 500) {
+        pins.digitalWritePin(DigitalPin.P12, 1)
+    } else if (kontroller > 550) {
+        pins.digitalWritePin(DigitalPin.P15, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    }
 })
