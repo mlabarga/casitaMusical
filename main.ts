932c62e22aa3c8af60a169a9a9e60be3ddd5a2af
mayte <mayte_labarga@hotmail.com>
2023-01-08 15:05:22 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,13 +6,20 @@ basic.forever(function () {
     kontroller = pins.analogReadPin(AnalogPin.P0)
     if (kontroller < 200) {
         pins.digitalWritePin(DigitalPin.P2, 1)
-    } else if (kontroller >= 220 && kontroller <= 500) {
+        music.playTone(440, music.beat(BeatFraction.Sixteenth))
+        basic.pause(20)
+    } else if (kontroller > 250 && kontroller <= 500) {
         pins.digitalWritePin(DigitalPin.P12, 1)
+        music.playTone(698, music.beat(BeatFraction.Sixteenth))
+        basic.pause(20)
     } else if (kontroller > 550) {
         pins.digitalWritePin(DigitalPin.P15, 1)
+        music.playTone(175, music.beat(BeatFraction.Sixteenth))
+        basic.pause(20)
     } else {
         pins.digitalWritePin(DigitalPin.P2, 0)
         pins.digitalWritePin(DigitalPin.P12, 0)
         pins.digitalWritePin(DigitalPin.P15, 0)
+        music.stopAllSounds()
     }
 })
